c87691a89099a55372b7194c5917625e076ef5cb
jul-ian <julianaviles@gmail.com>
2021-03-16 23:06:45 -0700

    Still working on final script

--- model_script.R
+++ model_script.R
@@ -25,64 +25,39 @@ all_same_char <- function(s) {
 posts <- read_csv("post_data.csv") %>% 
   rename_all(tolower) %>% 
   rename(ori_rate="y") %>% 
-  mutate(line = row_number(), rating = gsub("_.+", "", ori_rate), body = paste(title, body))
+  mutate(line = row_number(), rating = gsub("_.+", "", ori_rate), text = paste(title, body)) %>% 
+  mutate(text = gsub("[^ -~]", "", text)) %>% 
+  mutate(text = gsub("<[a-z]+>", "", text)) %>% 
+  mutate(tags = tag_extract(tags)) %>% 
+  mutate(text = paste(text, tags))
 
 posts_tidy <- posts %>% 
-  mutate(body = gsub("[^ -~]", "", body)) %>% 
-  mutate(body = gsub("<[a-z]+>", "", body)) %>% 
-  mutate(tags = tag_extract(tags)) %>% 
-  mutate(body = paste(body, tags)) %>% 
-  unnest_tokens(word, body) %>% 
+  unnest_tokens(word, text) %>% 
   select(line, word, rating) %>% 
   anti_join(stop_words, by = "word") %>% 
   group_by(line, word, rating) %>% 
   summarise(n = n()) %>% 
   filter(grepl("[[:alpha:]|\\+\\+|#]", word)) %>% 
-  filter(!grepl("[[:digit:]]", word)) %>% 
+  filter(!grepl("[[:digit:]]+", word)) %>% 
   filter(!all_same_char(word)) %>% 
   filter(nchar(word) < 15)
-  
+
+posts_freqs <- posts_tidy %>% 
+  group_by(rating, word) %>% 
+  summarise(n_total = sum(n)) %>% 
+  arrange(desc(n_total))
+
+## add this plot later
+posts_freqs %>% 
+  top_n(15) %>% 
+  arrange(n_total) %>%
+  ggplot(aes(x = word, y = n_total, fill = rating)) + 
+  geom_bar(stat = "identity") +
+  ylab("Frequency") +
+  coord_flip() +
+  facet_wrap(~rating, scale = "free")
+
 posts_dtm <- cast_dtm(posts_tidy, line, word, n)
-
-#print(inspect(posts_dtm))
-
-# posts_tidy <- posts %>% 
-#   unnest_tokens(word, body) %>% 
-#   select(word, rating) %>% 
-#   group_by(word, rating) %>% 
-#   summarise(n = n()) %>% 
-#   filter(!grepl("[[:punct:]|[:digit:]]", word), nchar(word) > 2) %>% 
-#   filter(n > 5) %>% 
-#   anti_join(stop_words, by = "word")
-
-# test1 <- posts %>% 
-#   mutate(line = row_number()) %>% 
-#   unnest_tokens(word, body) %>% 
-#   select(line, word, rating) %>% 
-#   group_by(line, word, rating) %>% 
-#   summarise(n = n()) %>% 
-#   filter(!grepl("[[:punct:]|[:digit:]]", word), nchar(word) > 2) %>% 
-#   filter(n > 5) %>% 
-#   anti_join(stop_words, by = "word")
-
-
-#add_stop_words <- posts_tidy$word[c(1:48, (nrow(posts_tidy) - 2):nrow(posts_tidy))]
-
-#posts_tidy <- posts_tidy[49:(nrow(posts_tidy) - 2), ]
-
-# posts_corpus <- Corpus(VectorSource(posts$body)) %>%
-#   tm_map(PlainTextDocument) %>%
-#   tm_map(tolower) %>%
-#   tm_map(removePunctuation) %>%
-#   tm_map(removeNumbers) %>%
-#   tm_map(removeWords, c(stopwords('en'), add_stop_words)) %>%
-#   tm_map(stemDocument)
-
-# posts_dtm <- DocumentTermMatrix(posts_corpus)
-# posts_dtm2 <- posts_tidy %>% 
-#   ungroup() %>% 
-#   mutate(line = row_number()) %>% 
-#   cast_dtm(line, word, n)
 
 posts_sparse <- removeSparseTerms(posts_dtm, 0.99)
 posts_df <- as_tibble(as.matrix(posts_sparse))
@@ -107,20 +82,3 @@ sum(posts_test$post_rating == preds)/length(posts_test$post_rating)
 # knnmod <- knn(train = posts_train[, colnames(posts_train) != "post_rating"], 
 #               test = posts_test[, colnames(posts_test) != "post_rating"], 
 #               cl = posts_train$post_rating, k = 1)
-
-
-
-  
-posts_tidy %>%
-  group_by(rating) %>%
-  top_n(10, word) %>%
-  ungroup() %>% 
-  arrange(rating, -word)
-
-
-  mutate(term = reorder_within(term, beta, topic)) %>%
-  ggplot(aes(term, beta, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free") +
-  coord_flip() +
-  scale_x_reordered()
