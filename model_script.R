a127619212ef3c9eaeaa266c3eb06fd1f6cf83a9
jul-ian <julianaviles@gmail.com>
2021-03-18 00:51:22 -0700

    modified cleaning steps, added lda analysis

--- model_script.R
+++ model_script.R
@@ -9,6 +9,7 @@ library(caTools)
 library(class)
 library(tidytext)
 library(ggplot2)
+library(wordcloud)
 
 tag_extract <- function(tag) {
   tag %>% 
@@ -27,7 +28,8 @@ posts <- read_csv("post_data.csv") %>%
   rename(ori_rate="y") %>% 
   mutate(line = row_number(), rating = gsub("_.+", "", ori_rate), text = paste(title, body)) %>% 
   mutate(text = gsub("[^ -~]", "", text)) %>% 
-  mutate(text = gsub("<[a-z]+>", "", text)) %>% 
+  mutate(text = gsub("</?[a-z]+>", "", text)) %>% 
+  mutate(text = gsub("&lt|&gt|<a href=https|rel=", "", text)) %>% 
   mutate(tags = tag_extract(tags)) %>% 
   mutate(text = paste(text, tags))
 
@@ -45,10 +47,21 @@ posts_tidy <- posts %>%
 posts_freqs <- posts_tidy %>% 
   group_by(rating, word) %>% 
   summarise(n_total = sum(n)) %>% 
-  arrange(desc(n_total))
+  arrange(desc(n_total)) %>% 
+  ungroup()
 
-## add this plot later
+## add this plot later, try to change order of levels
+## frequency of unique words
 posts_freqs %>% 
+  arrange(desc(n_total)) %>% 
+  head(19) %>% 
+  ggplot(aes(x = word, y = n_total)) + 
+  geom_bar(stat = "identity") +
+  ylab("Frequency") +
+  coord_flip()
+
+posts_freqs %>% 
+  group_by(rating) %>% 
   top_n(15) %>% 
   arrange(n_total) %>%
   ggplot(aes(x = word, y = n_total, fill = rating)) + 
@@ -56,10 +69,77 @@ posts_freqs %>%
   ylab("Frequency") +
   coord_flip() +
   facet_wrap(~rating, scale = "free")
+##
+
+## wordcloud plots ##
+posts_freqs %>% 
+  select(word, n_total) %>% 
+  arrange(desc(n_total)) %>% 
+  head(15) %>% 
+  wordcloud2(., color="random-dark")
+
+posts_freqs %>% 
+  group_by(rating) %>% 
+  top_n(15) %>% 
+  ungroup() %>% 
+  filter(rating == "HQ") %>% 
+  select(word, n_total) %>% 
+  arrange(desc(n_total)) %>% 
+  wordcloud2(., color="random-dark")
+
+posts_freqs %>% 
+  group_by(rating) %>% 
+  top_n(15) %>% 
+  ungroup() %>% 
+  filter(rating == "LQ") %>% 
+  select(word, n_total) %>% 
+  arrange(desc(n_total)) %>% 
+  wordcloud2(., color="random-dark")
+##
+
+## topic modelling ##
+posts_dtm <- cast_dtm(posts_tidy, line, word, n)
+#posts_sparse <- removeSparseTerms(posts_dtm, 0.99)
+
+lda_mod <- LDA(posts_dtm, k = 2, control = list(seed = 888))
+lda_topics <- tidy(lda_mod, matrix = "beta")
+
+posts_top_terms <- lda_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>% 
+  arrange(topic, -beta)
+posts_top_terms %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip() +
+  scale_x_reordered()
+
+lda_mod <- LDA(posts_dtm, k = 2, control = list(seed = 888))
+lda_topics <- tidy(lda_mod, matrix = "beta")
+
+posts_top_terms <- lda_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>% 
+  arrange(topic, -beta)
+posts_top_terms %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip() +
+  scale_x_reordered()
+##
+
+
+
 
 posts_dtm <- cast_dtm(posts_tidy, line, word, n)
-
 posts_sparse <- removeSparseTerms(posts_dtm, 0.99)
+
 posts_df <- as_tibble(as.matrix(posts_sparse))
 
 posts_df <- as_tibble(as.matrix(posts_sparse)) %>%
